248d95b0d2a3aa15253f1fdbfd79fdf720d10095
Guichaguri <guichaguri@gmail.com>
2016-11-27 14:29:07 -0200

    Updated to 1.11

--- src/main/java/me/guichaguri/tickratechanger/TickrateChanger.java
+++ src/main/java/me/guichaguri/tickratechanger/TickrateChanger.java
@@ -29,7 +29,7 @@ public class TickrateChanger implements IFMLLoadingPlugin, IFMLCallHook {
     public static File CONFIG_FILE = null;
 
     public static final String MODID = "tickratechanger";
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.9";
 
     public static final String GAME_RULE = "tickrate";
 
